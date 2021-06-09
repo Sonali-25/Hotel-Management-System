9d474e8ed881bfefa648023403e38f1d4ba849b4
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-09 13:27:59 +0530

    Welcome To My Hotel Management System Program

---
+++ src/main/java/com/magic/workshop2/Main.java
@@ -0,0 +1,7 @@
+package com.magic.workshop2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To My Hotel Reservation System Program");
+    }
+}
